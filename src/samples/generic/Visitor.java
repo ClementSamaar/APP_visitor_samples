88d4ca233006d5a66a6d39548af6a1f68cb98be8
Samaar Clément <77165371+ClementSamaar@users.noreply.github.com>
2022-11-15 17:07:16 +0100

    Added : Shape sample

--- src/samples/generic/Visitor.java
+++ src/samples/generic/Visitor.java
@@ -1,6 +1,6 @@
 package samples.generic;
 
 public interface Visitor {
-    public void visitObjectA(ObjectA objectA);
-    public void visitObjectB(ObjectB objectB);
+    void visitObjectA(ObjectA objectA);
+    void visitObjectB(ObjectB objectB);
 }
